2657faea0c93fa07b1b8819c9d7295859f61f9c6
meshechko <79574011+meshechko@users.noreply.github.com>
2021-07-20 10:50:23 +1200

    Update main.js

--- static/js/main.js
+++ static/js/main.js
@@ -3,17 +3,13 @@
     $('[data-toggle="tooltip"]').tooltip()
   })
 
-var page_y = $( document ).scrollTop();
-window.location.href = window.location.href + '?page_y=' + page_y;
+$(window).scroll(function() {
+  sessionStorage.scrollTop = $(this).scrollTop();
+});
 
-$(function() {
-    if ( window.location.href.indexOf( 'page_y' ) != -1 ) {
-        //gets the number from end of url
-        var match = window.location.href.split('?')[1].match( /\d+$/ );
-        var page_y = match[0];
-
-        //sets the page offset 
-        $( 'html, body' ).scrollTop( page_y );
-    }
+$(document).ready(function() {
+  if (sessionStorage.scrollTop != "undefined") {
+    $(window).scrollTop(sessionStorage.scrollTop);
+  }
 });
   
